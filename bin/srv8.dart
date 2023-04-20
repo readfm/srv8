dfaea3068fb587ef164577663302f0fd1fa94719
Mantas <mntask@gmail.com>
2023-04-20 16:49:50 -0400

    better images and server

--- bin/srv8.dart
+++ bin/srv8.dart
@@ -1,6 +1,9 @@
+import 'dart:ffi';
+import 'package:data8/index.dart';
 import 'package:srv8/server.dart';
 import 'package:srv8/srv8.dart' as srv8;
 
 void main(List<String> arguments) {
+  FData.path = '../data/';
   FServer();
 }
--- lib/server.dart
+++ lib/server.dart
@@ -5,6 +5,8 @@ import 'package:mime/mime.dart';
 
 import 'package:crypto/crypto.dart';
 import 'package:path/path.dart';
+import 'package:data8/index.dart';
+import 'package:data8/utils/random.dart';
 
 class FServer {
   static const path = '../';
@@ -12,131 +14,137 @@ class FServer {
 
   listen() async {
     var server = await HttpServer.bind(
-      '0.0.0.0',
+      host,
       port,
     );
 
     print('Listening on ${server.address.host}:${server.port}');
 
-    server.listen((HttpRequest req) async {
-      print(
-        'Request ${req.uri.path} by ${req.headers['X-Forwarded-For']}',
-      );
-
-      if (req.uri.path.startsWith('/uploads/')) {
-        final hash = basename(req.uri.path);
-
-        final f = File('$uploadDir/$hash');
-
-        if (f.existsSync()) {
-          final bytes = f.readAsBytesSync();
-          //final mimeType = lookupMimeType(hash);
-          req.response
-            //..headers.contentType = ContentType.parse(mimeType!)
-            ..add(bytes)
-            ..close();
-        } else {
-          req.response
-            ..statusCode = HttpStatus.notFound
-            ..write('File not found')
-            ..close();
-        }
-        return;
-      }
-
-      if (req.uri.path.startsWith('/upload')) {
-        List<int> dataBytes = [];
-
-        await for (var data in req) {
-          dataBytes.addAll(data);
-        }
-
-        final boundary = req.headers.contentType!.parameters['boundary'];
-        final transformer = MimeMultipartTransformer(boundary!);
-
-        final bodyStream = Stream.fromIterable([dataBytes]);
-        final parts = await transformer.bind(bodyStream).toList();
-        dataBytes.clear();
-
-        if (!Directory(uploadDir).existsSync()) {
-          await Directory(uploadDir).create();
-        }
-
-        final bytes = <int>[];
-        for (var part in parts) {
-          final content = await part.toList();
-          bytes.addAll(content[0]);
-        }
-        parts.clear();
-
-        final hash = sha256.convert(bytes).toString();
-        final fw = File('$uploadDir/$hash');
-        fw.writeAsBytes(bytes);
-
-        // Send a response to the client
-        req.response
-          ..statusCode = HttpStatus.ok
-          ..write('File uploaded successfully')
-          ..close();
-        return;
-      }
-
-      // responds with files parent directory
-      final host = req.headers['host']!.first.split(':')[0];
-      final f = File(
-        '$path/sites/${host}/' + req.uri.path.replaceAll('..', ''),
-      );
-
-      final stat = f.statSync();
-      if (f.existsSync() && stat.type == FileSystemEntityType.file) {
-        final bytes = f.readAsBytesSync();
-        final mimeType = lookupMimeType(f.path);
-        req.response
-          ..headers.contentType = ContentType.parse(mimeType!)
-          ..add(bytes)
-          ..close();
-      } else {
-        final fIndex = File(
-          '$path/sites/${host}/index.html',
+    server.listen(
+      (HttpRequest req) async {
+        print(
+          'Request ${req.uri.path} by ${req.connectionInfo?.remoteAddress ?? req.headers['X-Forwarded-For']}',
         );
-        req.response
-          ..headers.contentType = ContentType.html
-          //..statusCode = HttpStatus.notFound
-          ..write(fIndex.readAsStringSync())
-          ..close();
-      }
 
-      /*
-      if (req.uri.path.startsWith('/')) {
-        final connection = await WebSocketTransformer.upgrade(req);
-        final socket = FSocket();
+        if (req.uri.path.startsWith('/uploads/')) {
+          final hash = basename(req.uri.path);
 
-        final name = getRandomString(5);
+          final f = File('$uploadDir/$hash');
 
-        sockets[name] = socket;
-
-        distributor(Map<String, dynamic> msg) {
-          for (final s in sockets.entries) {
-            if (s.key == name) continue;
-            s.value.sink(msg);
+          if (f.existsSync()) {
+            final bytes = f.readAsBytesSync();
+            final mimeType = 'application/octet-stream';
+            //lookupMimeType(hash);
+            req.response
+              ..headers.contentType = ContentType.parse(mimeType)
+              ..add(bytes)
+              ..close();
+          } else {
+            req.response
+              ..statusCode = HttpStatus.notFound
+              ..write('File not found')
+              ..close();
           }
+          return;
         }
 
-        // send messages to the client
-        socket.stream.listen((d) {
-          if (connection.readyState == WebSocket.open) {
-            if (d is Map<String, dynamic>) {
-              final r = connection.add(jsonEncode(d));
-            }
-          } else {
-            // Remove socket if the connection is already closed
-            sockets.remove(name);
+        if (req.uri.path.startsWith('/upload')) {
+          try {
+            upload(req);
+          } catch (e) {
+            print(e);
+            req.response
+              ..statusCode = HttpStatus.internalServerError
+              ..write('Error')
+              ..close();
           }
-        });
+          return;
+        }
 
-        // receive messages from the client
-        connection.listen((d) async {
-          print('received: $d');
+        if (req.uri.path.startsWith('/')) {
+          socket(req);
+          //socket.ready(this);
+          // distribute messages to the sockets
+          //Communication.catalog.doListen(distributor);
+        }
+      },
+      onError: (e) {
+        print('Error: $e');
+      },
+      cancelOnError: false,
+    );
+  }
+
+  upload(HttpRequest req) async {
+    List<int> dataBytes = [];
+
+    await for (var data in req) {
+      dataBytes.addAll(data);
+    }
+
+    final boundary = req.headers.contentType!.parameters['boundary'];
+    final transformer = MimeMultipartTransformer(boundary!);
+
+    final bodyStream = Stream.fromIterable([dataBytes]);
+    final parts = await transformer.bind(bodyStream).toList();
+    dataBytes.clear();
+
+    if (!Directory(uploadDir).existsSync()) {
+      await Directory(uploadDir).create();
+    }
+
+    final bytes = <int>[];
+    for (var part in parts) {
+      final content = await part.toList();
+      bytes.addAll(content[0]);
+    }
+    parts.clear();
+
+    final hash = sha256.convert(bytes).toString();
+    final fw = File('$uploadDir/$hash');
+    fw.writeAsBytes(bytes);
+
+    // Send a response to the client
+    req.response
+      ..statusCode = HttpStatus.ok
+      ..write('File uploaded successfully')
+      ..close();
+  }
+
+  socket(HttpRequest req) async {
+    final name = getRandomString(5);
+    final connection = await WebSocketTransformer.upgrade(req);
+    final socket = FSocket(
+      name: name,
+      spread: spread,
+    );
+
+    sockets[name] = socket;
+
+    // send messages to the client
+    socket.stream.listen(
+      (d) {
+        if (connection.readyState == WebSocket.open) {
+          if (d is Map<String, dynamic>) {
+            final r = connection.add(jsonEncode(d));
+          }
+        } else {
+          // Remove socket if the connection is already closed
+          sockets.remove(name);
+        }
+      },
+      onError: (e) {
+        print('socket error: $e');
+        sockets.remove(name);
+      },
+      cancelOnError: false,
+    );
+
+    // receive messages from the client
+    connection.listen(
+      (d) async {
+        print('received: $d');
+        try {
           final fractal = socket.receive(d);
 
           /*
@@ -151,23 +159,30 @@ class FServer {
             print('session');
             fractal.handle(socket.session!);
           }
-        }, onDone: () {
-          socket.active = false;
-          sockets.remove(name);
-          //Communication.catalog.unListen(distributor);
-          connection.close();
+        } catch (e) {
+          print('ws error: $e');
+        }
+      },
+      onDone: () {
+        socket.active = false;
+        sockets.remove(name);
+        //Communication.catalog.unListen(distributor);
+        connection.close();
+        print(
+          'Disconnected ${connection.closeCode}#${connection.closeReason}',
+        );
+      },
+      onError: (e) {
+        print('ws error: $e');
+      },
+    );
+  }
 
-          print(
-            'Disconnected ${connection.closeCode}#${connection.closeReason}',
-          );
-        });
-
-        //socket.ready(this);
-        // distribute messages to the sockets
-        //Communication.catalog.doListen(distributor);
-      }
-      */
-    });
+  notFound(HttpRequest req) {
+    req.response
+      ..statusCode = HttpStatus.notFound
+      ..write('File not found')
+      ..close();
   }
 
   static const local = '127.0.0.1';
@@ -180,9 +195,19 @@ class FServer {
     listen();
   }
 
+  final Map<String, FSocket> sockets = {};
+
   //final Map<String, FSocket> sockets = {};
 
-  distribute(msg) {}
+  spread(
+    Map<String, dynamic> msg, [
+    List<String> exclude = const [],
+  ]) {
+    for (final s in sockets.entries) {
+      if (exclude.contains(s.key)) continue;
+      s.value.sink(msg);
+    }
+  }
 
   @override
   handle(socket) {
